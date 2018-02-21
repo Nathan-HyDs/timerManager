0be3ddb21d8d5ee75f77d08b19324f992e957d43
hyds <hyds@Saturne.lan>
2018-02-21 22:10:04 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TimerCounter/>
+  </div>
+</template>
+
+<script>
+import TimerCounter from './components/TimerCounter'
+
+export default {
+  name: 'App',
+  components: {
+    TimerCounter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TimerCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+  test {{count}}
+  </div>
+</template>
+
+<script>
+import store from './TimerStore'
+import Vuex from 'vuex'
+
+export default {
+  store: store,
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    ...Vuex.mapGetters(['count'])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
